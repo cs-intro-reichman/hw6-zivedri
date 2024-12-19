e5a8dae742ae499c400319ae66c795c98e847fa3
ziveedri <ziv.edri01@runi.ac.il>
2024-12-19 19:24:07 +0200

    Done , first try.

--- Editor2.java
+++ Editor2.java
@@ -10,7 +10,21 @@ import java.awt.Color;
 public class Editor2 {
 
 	public static void main (String[] args){
-		//// Replace this comment with your code.
-		//// This function is similar to the main function of Editor1.java			
+		String fileName = args[0];
+		int width = Integer.parseInt(args[1]);
+		int height = Integer.parseInt(args[2]);
+		// Reads the input image and creates an empty output image
+		Color[][] imageIn = Runigram.read(fileName);
+		Color[][] imageOut = Runigram.scaled(imageIn , width , height);
+		// Applies the specified image processing function
+		// Creates a canvas in which both images will be displayed, one after the other.
+		// Next, displays the input image, and pauses for a few seconds.
+		// Finally, displays the output image.
+		// (Notice that both images have the same dimensions).
+		Runigram.setCanvas(imageIn);
+		Runigram.display(imageIn);
+		StdDraw.pause(3000);
+		Runigram.setCanvas(imageIn);
+		Runigram.display(imageOut);
 	}
 }
